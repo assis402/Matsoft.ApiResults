38c5ed09cc212554aaa2eebd7c3ef4311d072884
Matheus de Assis <assis4002@gmail.com>
2023-10-02 11:11:34 -0300

    feat: add result method with exception

---
+++ src/ApiResults/ExceptionData.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiResults;
+
+public readonly record struct ExceptionData
+{
+    public ExceptionData(Exception exception)
+    {
+        ErrorMessage = exception.Message;
+        StackTrace = exception.StackTrace;
+        InnerException = exception.InnerException;
+    }
+
+    public readonly string ErrorMessage { get; }
+    public readonly string StackTrace { get; }
+    public readonly Exception InnerException { get; }
+}
